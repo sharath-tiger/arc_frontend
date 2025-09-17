9a9877fab14a577d47f89f73ecf32c450eb6c85e
Irish K Sajan <irishk.sajan@tigeranalytics.com>
2025-09-17 18:18:48 +0530

    Updated ResultsPage.tsx

--- src/components/ResultsPage.tsx
+++ src/components/ResultsPage.tsx
@@ -203,7 +203,7 @@ function ResultsPage() {
                 {/* Payback Period */}
                 <div className="space-y-2">
                   <label className="block text-sm font-semibold text-gray-700">
-                    Maximum Payback Period (No. of Months) *
+                    Minimum Payback Period (No. of Months) *
                   </label>
                   <input
                     type="number"

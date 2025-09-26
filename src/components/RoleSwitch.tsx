650db448aa3e07506a4f491e916ad1d1fe294fdf
Sharath Mohan <sharath.mohan@tigeranalytics.com>
2025-09-26 17:25:54 +0530

    fx: overlay issue fixed

--- src/components/RoleSwitch.tsx
+++ src/components/RoleSwitch.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function RoleSwitch() {
   return (
-    <div className="fixed bottom-4 right-4 bg-lime-100 p-4 rounded-lg shadow-lg flex items-center">
+    <div className="fixed bottom-10 left-4 bg-lime-100 p-4 rounded-lg shadow-lg flex items-center">
       <Link to="/admin-settings">admin</Link>
       <Link to="/list-campaigns" className="ml-4">
         analyst

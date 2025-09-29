66d617a8946040a8fd0d60271f33515206bd1f82
Irish K Sajan <irishk.sajan@tigeranalytics.com>
2025-09-29 13:03:20 +0530

    Updated LoanDetailsPage.tsx

--- src/components/LoanDetailsPage.tsx
+++ src/components/LoanDetailsPage.tsx
@@ -123,7 +123,6 @@ function LoanDetailsPage() {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
-      minimumFractionDigits: 0,
       maximumFractionDigits: 0,
     }).format(amount);
   };
